079ac12713a1f9476c72bde3c1112a53148ca037
Joël <jgrimberg@qxperts.io>
2022-04-07 22:06:37 +0200

    fix module paths in jest config

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   },
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
-  modulePaths: ['<rootDir>'],
+  modulePaths: ['<rootDir>', '<rootDir>/src', '<rootDir>/src/utils'],
   rootDir: '.',
   moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'utils'],
 }
