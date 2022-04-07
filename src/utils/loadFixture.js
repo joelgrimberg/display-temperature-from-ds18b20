e72d4e0a05e87e2594613166a4d9472620585472
Joël <jgrimberg@qxperts.io>
2022-04-07 20:25:43 +0200

    tests

---
+++ src/utils/loadFixture.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+function loadFixture(filename) {
+  return fs.readFileSync(path.join(__dirname, '__fixtures__', filename), 'utf8')
+}
+
+module.exports = loadFixture
--- src/utils/readtemp.test.js
+++ src/utils/readtemp.test.js
@@ -1,14 +1,8 @@
-const readTemp = require('./readtemp.js')
+const readTemp = require('./readTemp.js')
+const loadFixture = require('./loadFixture.js')
 
-// import tempReading from '__fixtures__ /temp1.txt'
-const fs = require('fs')
-const path = require('path')
-
-const file = path.join(__dirname, '__fixtures__', 'temp1.txt')
-const fdr = fs.readFileSync(file, 'utf8', function (err, data) {
-  return data
-})
+const fdr = loadFixture('temp.txt')
 
 test('returns temp', () => {
-  expect(readTemp(fdr)).toBe(18.125)
+  expect(readTemp(fdr)).toBe(18.725)
 })
