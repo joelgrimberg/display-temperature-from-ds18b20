91b4a99ca6305471eb9e5ee745c1156c415be497
Joël <joel@joelgrimberg.nl>
2023-12-05 20:45:30 +0100

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,42 @@
-import file from '../../public/temp.txt'
-import readTemp from '../utils/readtemp'
+import { useState, useEffect, useMemo } from 'react';
+import { useRouter } from 'next/router';
 
-var x = readTemp(file)
+const temp = { x: 1 };
 
-const Index = () => (
-  <div>
-    <h1>🌡️ {x} 🌡️</h1>
-    <h2></h2>
-  </div>
-)
+export const getServerSideProps = async (context) => {
+  console.log(process.platform);
+  if (process.env.SETUP === 'raspberry_pi') {
+    var sensor = require('ds18x20');
+    var temp = sensor.get(process.env.DS18B20_SENSOR);
+    return {
+      props: { temp: temp },
+    };
+  }
+  if (process.env.SETUP === 'macos') {
+    return {
+      props: { x: 2.22 },
+    };
+  } else {
+    return {
+      props: { x: 'no environment variable set' },
+    };
+  }
+};
+export default function Index(props) {
+  const router = useRouter();
+  console.log(props);
+  const refreshData = () => {
+    router.replace(router.asPath, undefined, { shallow: true });
+  };
 
-export default Index
+  useEffect(() => {
+    setInterval(refreshData, 15_000);
+  });
+
+  return (
+    <div>
+      <h1>🌡️ {props.x}</h1>
+      <h2></h2>
+    </div>
+  );
+}
