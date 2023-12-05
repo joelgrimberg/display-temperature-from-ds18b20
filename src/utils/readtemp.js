6d6db2f953e3d532dfb15fae17fde5fa2925b2b7
Joël <joel@joelgrimberg.nl>
2023-12-05 15:11:49 +0100

    read temperature directly from gpio

--- src/utils/readtemp.js
+++ src/utils/readtemp.js
@@ -1,6 +1,13 @@
 function readTemp(input) {
-  const str = input.split(' ')
-  return str[20].slice(2) / 1000.0
+  try {
+    const str = input.split(' ');
+    return str[20].slice(2) / 1000.0;
+  } catch (err) {
+    console.log('tried to slice a string', err);
+    console.log('input', input);
+  }
+  const str = input.split(' ');
+  return str[20].slice(2) / 1000.0;
 }
 
-module.exports = readTemp
+module.exports = readTemp;
